1e7bcab48d8f1fd809b53785e485b5f81fd2beee
seanjungmsba <seanjeong1996@gmail.com>
2022-06-23 12:44:22 -0400

    Update

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -15,16 +15,16 @@ public class Main {
             System.out.println("You cannot be less than 0 year old!");
             System.exit(1);
         }
-        
 
-        if (age >= 18) {
-            System.out.println("Your age is more than 18. I expect you to act your age.");
+        if (age > 18) {
+            System.out.println("Your are more than 18 years old. You are an adult.");
+        } else if (age == 18) {
+            System.out.println("You are exactly 18 years old. I am assuming you recently graduated from a high school.");
         } else if (age < 18 && age >= 10) {
-            System.out.println("Your age is between 10 and 18, so you must be in a school.");
+            System.out.println("You are older than 10 years old and but younger than 18 years old, so you must be in a school.");
         } else {
-            System.out.println("You are less than 9 years old? Hello Kido!");
+            System.out.println("You are less than 9 years old? Hello Kido! Watch some Baby Shark.");
         }
-
 
     }
 }
